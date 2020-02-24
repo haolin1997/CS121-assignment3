535d882efee2564935dc3ac681659e66e311c5d3
frankcx95 <xuc11@uci.edu>
2020-02-24 10:26:28 -0800

    update

--- M1.py
+++ M1.py
@@ -22,11 +22,15 @@ class InvertedIndex():
             posting = Posting(url, word, count)
             self.map[word].append(posting)
 
-    def print_index(self):
+    def print_index(self, file, size):
         """ print out the index """
-        for word, posting in self.map.items():
-            print (word + " -> " + str([p.get_posting() for p in posting]))
+        f = open('M1_Report.txt', 'a+')
+        f.write("total_unique_tokens: " + str(len(self.map))+ '\n')
+        f.write("total_unique_documents: " + str(i) + '\n')
+        #for word, posting in self.map.items():
+            #print(str([p.get_posting() for p in posting]))
 
+        f.close()
 
 
 class Posting():
@@ -115,10 +119,16 @@ if __name__ == "__main__":
 
     #json_file = '{"url": "https://www.cs.uci.edu/event/c-mohan-2/?ical=1", "content": "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//Department of Computer Science - Donald Bren School of Information &amp; Computer Sciences - ECPv4.6.14.1//NONSGML v1.0//EN\r\nCALSCALE:GREGORIAN\r\nMETHOD:PUBLISH\r\nX-WR-CALNAME:Department of Computer Science - Donald Bren School of Information &amp; Computer Sciences\r\nX-ORIGINAL-URL:https://www.cs.uci.edu\r\nX-WR-CALDESC:Events for Department of Computer Science - Donald Bren School of Information &amp; Computer Sciences\r\nBEGIN:VEVENT\r\nDTSTART;VALUE=DATE:20150501\r\nDTEND;VALUE=DATE:20150502\r\nDTSTAMP:20191013T140443\r\nCREATED:20180501T070348Z\r\nLAST-MODIFIED:20180501T070927Z\r\nUID:1491-1430438400-1430524799@www.cs.uci.edu\r\nSUMMARY:C. MOHAN\r\nDESCRIPTION:Speaker: C. MOHAN (IBM Almaden Research Center) \\nHost: Michael Carey & Sharad Mehrotra \\nTitle: Big Data: Hype and Reality & Modern Database Systems: Modernized Classic Systems\\, NewSQL and NoSQL \\n\r\nURL:https://www.cs.uci.edu/event/c-mohan-2/\r\nCATEGORIES:Distinguished Lecture Series\r\nEND:VEVENT\r\nEND:VCALENDAR", "encoding": "ascii"}'
     #index.fetch_content(json_file)
-
-    path = "/Users/hamster/Documents/visualstudio-workspace/Python/cs121/CS121-assignment3/ANALYST/www_cs_uci_edu/0a0056fb9a53ec6f190aa2b5fb1a97c33cd69726c8841f89d24fa5abd84d276c.json"
-    json_file = open(path).readlines()[0]
-    index.fetch_content(json_file)
+    i = 0
+    for directory in os.listdir('DEV'):
+        if directory != '.DS_Store':
+            for filenames in os.listdir('DEV/' + directory):
+                path = 'DEV/'+ directory + '/' + filenames
+                json_file = open(path).readlines()[0]
+                print(path)
+                index.fetch_content(json_file)
+                i += 1
     
-    index.print_index()
+    f = open('M1_Report.txt', 'w')
+    index.print_index('M1_Report.txt', i)
     
